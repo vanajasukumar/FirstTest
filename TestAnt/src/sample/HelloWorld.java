8a074c3a7003b06d2fb8b0da9c97140b7966f032
vanaja <vanaja@192.168.1.109>
2014-07-27 09:39:58 -0400

    my first test

--- TestAnt/src/sample/HelloWorld.java
+++ TestAnt/src/sample/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Ant Scripting");
-// this is a test comment
+
+		
 	}
 
 }
